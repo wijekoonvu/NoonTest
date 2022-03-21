4110d7369b71fac4e037adc69c096e59f535bb92
User <wijekoonvinuri97@gmail.com>
2022-03-22 00:09:04 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Header from "../components/Header"
 import Feed from "../components/Feed"
 import Modal from "../components/Modal"
 
-
+//added home
 export default function Home(){
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide ">
